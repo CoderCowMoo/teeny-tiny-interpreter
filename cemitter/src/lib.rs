e314a897a8228ac63cac000cf08edcc26406e0c6
CoderCowMoo <broskichad38@gmail.com>
2022-12-02 19:24:54 +1100

    C Emitter finished

--- cemitter/src/lib.rs
+++ cemitter/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{fs::File, io::Write};
+
+struct Emitter {
+    full_path: String,
+    header: String,
+    code: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Emitter {
+    pub fn new(full_path: String) -> Self {
+        Emitter {
+            full_path,
+            header: "".to_string(),
+            code: "".to_string(),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn emit(&mut self, code: String) {
+        self.code.push_str(&code);
+    }
+
+    pub fn emit_line(&mut self, code: String) {
+        self.code.push_str(&code);
+        self.code.push('\n');
+    }
+
+    pub fn header_line(&mut self, code: String) {
+        self.header.push_str(&code);
+        self.header.push('\n');
+    }
+
+    pub fn write_file(&mut self) {
+        // 'borrowed' from sgmarz's implementation
+        if let Ok(mut f) = File::create(&self.full_path) {
+            // unwraps are kind of okay because error checking on outside
+            f.write_all(self.header.as_bytes()).unwrap();
+            f.write_all(self.code.as_bytes()).unwrap();
+            f.sync_all().unwrap();
+        } else {
+            panic!("Could not open file for writing: '{}'", self.full_path);
+        }
     }
 }
